387060dc4dc5b574b1cf9753794626e1ef3ed4f7
olegrewko <olegrewko@gmail.com>
2023-04-22 11:47:31 +0300

    begin

---
+++ docs/js/main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
